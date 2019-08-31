06bb28bbdd48d7e74e3d81da35d57069aa1c9f5c
ejaguirre <ejaguirre@cs104u-2d.acad.fullerton.edu>
2019-08-31 14:58:18 -0700

    created a calculator that generates from user input

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,29 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double cost, Tax, tip, bill;
+
+
+  //Get the cost of meal
+  std::cout << "Welcome to the Restaurant Bill Calculator!" << '\n';
+  std::cout << " What is the total meal cost? ";
+  std::cin >> cost;
+
+  //Calculate Tax
+  Tax= cost * .0775;
+  std::cout << " Tax:        " << Tax << '\n';
+
+  //Calculate tip
+  tip= cost * .2;
+  std::cout << " Tip:        " << tip << '\n';
+
+  //Calculate bill
+  bill= cost + Tax + tip;
+  std::cout << " Total bill: " << bill << '\n';
+
+  return 0;
+}
