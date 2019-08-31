b132175c3f73e98439e1aadccc3eadb9dc946c2b
ejaguirre <ejaguirre@cs104u-2d.acad.fullerton.edu>
2019-08-31 13:40:31 -0700

    displayed diamond pattern

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+//Prints diamond pattern to the console
+  std::cout << "   *   " << '\n';
+  std::cout << "  ***  " << '\n';
+  std::cout << " ***** " << '\n';
+  std::cout << "*******" << '\n';
+  std::cout << " ***** " << '\n';
+  std::cout << "  ***  " << '\n';
+  std::cout << "   *   " << '\n';
 
   return 0;
 }
